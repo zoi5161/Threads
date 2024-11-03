3aeccb7a6de46e7bc9cb9164610a7a9b3c59281e
Tran Bao <baomun250@gmail.com>
2024-11-03 22:05:42 +0700

    Update setting menu

--- frontend/src/public/js/naviLeft.js
+++ frontend/src/public/js/naviLeft.js
@@ -1,24 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
     const pinMenu = document.getElementById("pin-menu");
+    const setMenu = document.getElementById("setting-menu");
     const thumbtackIcon = document.getElementById("thumbtack-icon");
+    const barsStaggeredIcon = document.getElementById("bars-staggered-icon");
 
-    // Hiển thị hoặc ẩn menu khi nhấn vào biểu tượng thumbtack
-    thumbtackIcon.addEventListener("click", function (event) {
-        event.stopPropagation(); // Ngăn chặn sự kiện click lan ra ngoài
+    // Hàm hiển thị hoặc ẩn menu
+    function togglePinMenu() {
         if (pinMenu.style.display === "none" || pinMenu.style.display === "") {
-            // Hiển thị menu dưới dạng fixed khi nhấn vào thumbtack-icon
             pinMenu.style.display = "block";
             pinMenu.style.position = "fixed";
         } else {
-            // Ẩn menu khi nhấn lại vào thumbtack-icon
             pinMenu.style.display = "none";
         }
+    }
+
+    // Hàm hiển thị hoặc ẩn setting-menu
+    function toggleSettingMenu() {
+        if (setMenu.style.display === "none" || setMenu.style.display === "") {
+            setMenu.style.display = "block";
+            setMenu.style.position = "fixed";
+            pinMenu.style.display = "none"; // Ẩn pin-menu khi hiển thị setting-menu
+        } else {
+            setMenu.style.display = "none";
+        }
+    }
+
+    // Hiển thị hoặc ẩn menu khi nhấn vào thumbtack-icon
+    thumbtackIcon.addEventListener("click", function (event) {
+        event.stopPropagation();
+        togglePinMenu();
+    });
+
+    // Hiển thị hoặc ẩn menu khi nhấn vào bars-staggered-icon
+    barsStaggeredIcon.addEventListener("click", function (event) {
+        event.stopPropagation();
+        toggleSettingMenu();
     });
 
     // Ẩn menu khi nhấn vào bất kỳ vùng nào khác
     document.addEventListener("click", function (event) {
-        if (!pinMenu.contains(event.target) && event.target !== thumbtackIcon) {
+        if (!pinMenu.contains(event.target) && event.target !== thumbtackIcon && event.target !== barsStaggeredIcon) {
             pinMenu.style.display = "none";
+        }
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!setMenu.contains(event.target) && event.target !== thumbtackIcon && event.target !== barsStaggeredIcon) {
+            setMenu.style.display = "none";
         }
     });
 });
