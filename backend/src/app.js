b56cfebdb709445f2b790173261ed63c1fbfd42b
Trần Gia Bảo <68107603+zoi5161@users.noreply.github.com>
2024-11-27 10:12:32 +0700

    Fix app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const cors = require("cors"); //Prevent CORS errors
 const express = require("express"); //Use express
 const mongoose = require("mongoose"); //Use mongoose
 const routes = require("./routes/index"); //Connect to index.js in routes folder
-require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config({ path: "src/config/.env" });
 
 const app = express();
 
@@ -19,10 +19,10 @@ app.use(express.json()); //Use JSON
 app.use(routes); //Connect to index.js in routes folder
 
 const MONGODB_URI = process.env.MONGODB_URI; //Connect to MongoDB
-// mongoose.connect(MONGODB_URI)
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.error("MongoDB connection error:", err));
+mongoose.connect(MONGODB_URI)
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log("Connected to MongoDB successfully"))
+//     .catch(err => console.error("MongoDB connection error:", err));
     
 const PORT = process.env.PORT || 10000;
 
